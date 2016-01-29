5a751d1ca0ef32dc08f7c55e9e13d059376ec8c4
sha3er94 <mohamedelsha3er@icloud.com>
2016-01-29 17:18:36 +0200

    Initial Message

---
+++ Home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head> <title> Home</title> 
+<script src="jquery-1.11.3.min.js"></script>
+</head>
+<link rel="stylesheet" type="text/css" href="CSS.css" media="all" />
+<body>
+<div class="l"><a href="Home.php"><img src="logo.png"  display: "block" height="50" width="220"></a></div>
+<br/>
+<br/>
+	</body>
+<div class="container">
+            <div class="back-to-reg">
+              <a href="Shareholders.php" target="_self"><strong>Shareholders</strong></a>
+               <a href="Profile.php" target="_self"><strong>Company Profile</strong></a>
+               <a href="HSE.php" target="_self"><strong> HSE </strong>  </a>
+                <a href="Media.php" target="_self"><strong> Media Center </strong>  </a>
+                 <a href="Contact.php" target="_self"><strong> Contact Us </strong>  </a>
+                 <div class="clr">
+</div>
+ </div>
+</div>
+<br/>
+<div class="t"><img src="Picture1.jpg" height="450" width="600" align="right"> </div>
+ <p id="p01"> <Strong> Vision <Strong> </p>
+<p id="p02">To be a strong competitor in the gas derivatives field </p>
+   <p id="p01"> <Strong> Mission <Strong> </p>
+<p id="p02"> 
+	* Maximize propane and butane  production to fit the international & local market specifications safely, reliably and profitably.
+	<br/>
+	* Maintain ZERO HSE records
+	<br/>
+	* Sustain value and create strong financial returns for our shareholders and employees. 
+	<br/>
+	* Achieve highest staff performance </p>
+ <p id="p01"> <Strong>  Strategies <Strong> </p>
+<p id="p02"> 
+	* Reach the best performance in all dep. with a continuous monitoring and feedback to maximize the products.
+	* Apply high measures of process safety matching with the international stander KPI’s and best practices. 
+	Achieve business objectives by reducing costs and maximize product selling price.
+	Maintain plant equipment reliability and availability at highest level.
+	Develop the employees capabilities and build the leadership motivation through training.</p>
+
+</html>
---
+++ Shareholders.php
@@ -0,0 +1,21 @@
+<html>
+<head> <title> Shareholders</title> 
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
+<script>
+	$( "#egas" ).tooltip({ content: '<img src="EG.jpg">' });
+	$( "#apic" ).tooltip({ content: '<img src="API.jpg">' });
+	$( "#dana" ).tooltip({ content: '<img src="DANA.jpg">' });
+</script>
+</head>
+<link rel="stylesheet" type="text/css" href="CSS.css" media="all" />
+<body>
+<div class="l"><a href="Home.php"><img id="clip" src="logo.png"  display: "block" height="50" width="220"></a></div>
+<br/>
+<br/>
+<div class="t"><a href="Home.php"><img src="logo.png" height="80" width="100" align="middle"></a></div>
+<div class="t"><a id="egas" href="http://www.egas.com.eg/home.aspx"><img src="EGASDIAGRAM.png" usemap="#planetmap" height="80" width="100" align="middle"></a></div>
+<div class="t"><a id="apic" href="http://www.apicorp-arabia.com"><img src="APICORPDIAGRAM.png" usemap="#planetmap" height="80" width="100" align="middle"></a></div>
+<div class="t"><a id="dana" href="http://www.danagas.com/en"><img src="DANAGASDIAGRAM.png" usemap="#planetmap" height="80" width="100" align="middle"></a></div>
+</body>
+</html>
